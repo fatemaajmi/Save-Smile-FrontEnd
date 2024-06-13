1d908d9d54b8ac898893848be9bc4753c2f0cd05
Mohamed Husain <mo.abdulla3000@gmail.com>
2024-06-13 00:41:29 -0700

    creating routes and pages for home and about

---
+++ Save-and-Smile/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+const Home = () => {
+
+  return (
+    <div className="home-container col">         
+     
+      <section className="welcome-signin">
+
+       </section> 
+       
+        <section className="button">
+           <button>Click Here To Get Started</button>
+         </section>
+
+         <section className="style">
+
+         </section>
+
+           
+      </div>
+  )
+}
+
+export default Home
